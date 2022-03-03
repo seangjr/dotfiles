54b1ed8da2e1ca92a9d769b10803b83acea3ce39
szeanx <sean@relampagos.org>
2022-03-03 20:28:50 +0800

    edited arch packages

--- arch_packages/reinstall.sh
+++ arch_packages/reinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
 for pkgName in $(cat packages.txt)
 do
   sudo pacman -S --noconfirm $pkgName
